4430054185dd01e74a65a8b15b4d85d5fe86aae1
Tomer <Tomerlevy743@gmail.com>
2025-03-31 16:12:42 +0300

    rds output

--- terraform/modules/rds/output.tf
+++ terraform/modules/rds/output.tf
@@ -3,3 +3,15 @@ output "rds_endpoint" {
   value       = join("", regex("([^:]+)", aws_db_instance.statuspage_db.endpoint))
   # value       = join("", regex("([^:]+)", aws_db_instance.statuspage_db.endpoint))
 }
+output "db_name" {
+  description = "The name of the database"
+  value       = aws_db_instance.statuspage_db.db_name
+}
+output "db_user" {
+  description = "The username for the database"
+  value       = aws_db_instance.statuspage_db.username
+}
+output "db_password" {
+  description = "The password for the database"
+  value       = aws_db_instance.statuspage_db.password
+}
