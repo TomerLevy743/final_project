2432ace01616a2b93ccc7d8ead0026514a6642e2
guytamari <guy8267@gmail.com>
2025-03-31 18:59:49 +0300

    changes

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -2,5 +2,31 @@ resource "helm_release" "monitoring" {
   name      = "monitoring"
   namespace = "default"
   chart     = "../k8s-manifests/minikube-test/helm/monitoring-chart/"
-  values = [file("../k8s-manifests/minikube-test/helm/monitoring-chart/values.yaml")]
+
+  values = [yamlencode({
+    grafana = {
+      enabled  = true
+      service  = {
+        type = "ClusterIP"
+      }
+      ingress = {
+        enabled = false
+      }
+      ini = {
+        server = {
+          domain              = "status-page.org"
+          root_url            = "%(protocol)s://%(domain)s/grafana/"
+          serve_from_sub_path = true
+        }
+      }
+      persistence = {
+        enabled          = true
+        storageClassName = "gp2"
+        accessModes      = ["ReadWriteOnce"]
+        size             = "10Gi"
+      }
+      adminUser     = "admin"
+      adminPassword = "admin123"
+    }
+  })]
 }
